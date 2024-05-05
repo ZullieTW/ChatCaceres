f8585f03f547e383512939cdb0207741fb9428c2
ZullieTW <carlos92398@gmail.com>
2024-05-05 15:13:39 +0200

    Added activity_crear_perfil.xml and CrearPerfil.java

--- app/src/main/java/com/example/chatcceres/CrearPerfil.java
+++ app/src/main/java/com/example/chatcceres/CrearPerfil.java
@@ -1,8 +1,6 @@
 package com.example.chatcceres;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,25 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+public class CrearPerfil extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_crear_perfil);
 
-    }
-
-    public void onRegistrar(View view) {
-        //Comprobación entrada de datos
-
-
-
-
-
-        //Editar perfil
-        startActivity(new Intent(this, CrearPerfil.class));
-        this.finish();
     }
 }
